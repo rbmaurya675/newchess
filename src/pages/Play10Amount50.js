d1a602e6c6630b0cddac67e0b14602f47aeead8d
developerMaruya <rbmdev.jankiwebinfo@gmail.com>
2024-09-07 10:32:54 +0530

    Update Play10Amount50.js

--- src/pages/Play10Amount50.js
+++ src/pages/Play10Amount50.js
@@ -40,99 +40,90 @@ const Play10Amount50 = () => {
     return (
         <div className={styles.container}>
             <Slider {...settings} style={{ marginTop: '50px' }}>
+            <div className={styles.card} style={{ background: 'green' }}>
+            <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <img src="logo2.png" alt="Logo" style={{ height: "300px", justifyItems: 'center' }} />
+                <h3>Amount ₹ 30</h3>
+                <p>Lucknow</p>
+                <p>Time 10 Minutes</p>
+                <p>Play with an Online</p>
+            </Link>
+        </div>
                 <div className={styles.card} style={{ background: 'green' }}>
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
-                    {/* <FaChessPawn className={styles.icon} style={{ color: 'blue' }} /> */}
-                    {/* <img src="logo2.png" /> */}
-                    <h3>Amount ₹ 30</h3>
-                    <p>Lucknow</p>
-                    <p>time 10 Minutes</p>
-                    <p>Play with a Online </p>
-
-                </div>
-                <div className={styles.card} style={{ background: 'green' }}>
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
-                        <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
                     {/* <img src="logo2.png" /> */}
                     <h3>Amount ₹ 50</h3>
                     <p>Delhi</p>
                     <p>time 10 Minutes</p>
                     <p>Play with a Online </p>
-
+</Link>
                 </div>
 
                 <div className={styles.card}>
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
                     {/* <img src="logo2.png" /> */}
                     <h3>Amount ₹ 100</h3>
                     <p>Chanaini</p>
                     <p>time 10 Minutes</p>
                     <p>Play With a Online</p>
-
+</Link>
                 </div>
 
                 <div className={styles.card}>
-                    {/* <Link to="/practice/solo" style={{ textDecoration: 'none' }}>
-                        <img src="logo2.png" /> */}
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                   
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
+                  
                     <h3>Amount ₹ 200</h3>
                     <p>Kanpur</p>
                     <p>time 10 Minutes</p>
                     <p>Play With a Online</p>
-                    {/* </Link> */}
+                    </Link>
                 </div>
 
                 <div className={styles.card}>
-                    {/* <Link to="/practice/solo" style={{ textDecoration: 'none' }}>
-                        <img src="logo2.png" /> */}
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                    
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
+                   
                     <h3>Amount ₹ 500</h3>
                     <p>Mumbai</p>
                     <p>time 10 Minutes</p>
                     <p>Play With a Online</p>
-                    {/* </Link> */}
+                    </Link>
                 </div>
 
                 <div className={styles.card}>
-                    {/* <Link to="/practice/solo" style={{ textDecoration: 'none' }}>
-                        <img src="logo2.png" /> */}
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                    
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
+              
                     <h3>Amount ₹ 1000</h3>
                     <p>Agra</p>
                     <p>time 10 Minutes</p>
                     <p>Play With a Online</p>
-                    {/* </Link> */}
+                    </Link>
                 </div>
                 <div className={styles.card}>
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
+                   
                     <h3>Amount ₹ 1500</h3>
                     <p>Azamgarh</p>
                     <p>time 10 Minutes</p>
                     <p>Play With a Online</p>
-
+</Link>
                 </div>
                 <div className={styles.card}>
-                    <a href="https://chessboard.jankiwebinfotech.com/" style={{ textDecoration: 'none', backgroundColor: 'green' }}>
+                <Link to="/p2P" style={{ textDecoration: 'none', backgroundColor: 'green' }}>          
                         <img src="logo2.png" style={{ height: "300px", justifyItems: 'center' }} />
-                    </a>
                     <h3>Amount ₹ 5000</h3>
                     <p>time 10 Minutes</p>
                     <p>Allahabad</p>
                     <p>Play With a Online</p>
-
+</Link>
                 </div>
             </Slider>
         </div>
